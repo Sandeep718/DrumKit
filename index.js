a027337d84828fa443d7d38562c210f68f8a1d06
sandeep718 <nelantisandeep187@gmail.com>
2023-08-13 16:04:17 +0530

    added drumkit files

---
+++ index.js
@@ -0,0 +1,59 @@
+alert("sandeep's music");
+var num=document.querySelectorAll(".drum").length;
+for(var i=0;i<num;i++){
+    document.querySelectorAll(".drum")[i].addEventListener("click",function(){
+        var pressb=this.innerHTML;
+        makesound(pressb);
+        banni(pressb);
+    });
+}
+
+document.addEventListener("keydown",function(event){
+    makesound(event.key);
+    banni(event.key);
+})
+
+function banni(key1){
+    var press=document.querySelector("."+key1);
+    press.classList.add("pressed");
+
+    setTimeout(function(){
+        press.classList.remove("pressed");
+    }, 100);
+}
+
+function makesound(pressb){
+    switch (pressb) {
+        case "w":
+            var a1=new Audio("sounds/tom-1.mp3");
+            a1.play();
+            break;
+        case "a":
+            var a2=new Audio("sounds/tom-2.mp3");
+            a2.play();
+            break;
+        case "s":
+            var a3=new Audio("sounds/tom-3.mp3");
+            a3.play();
+            break;
+        case "d":
+            var a4=new Audio("sounds/tom-4.mp3");
+            a4.play();
+            break;
+        case "j":
+            var a5=new Audio("sounds/snare.mp3");
+            a5.play();
+            break;
+        case "k":
+            var a6=new Audio("sounds/kick-bass.mp3");
+            a6.play();
+            break;
+        case "l":
+            var a7=new Audio("sounds/crash.mp3");
+            a7.play();
+            break;
+        default:
+            console.log(pressb);
+            break;
+    }
+}
